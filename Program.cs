d1c6d600aaebff75ac0e4bbde2e23516c4b6870c
r1lUnreal <150723650+r1lUnreal@users.noreply.github.com>
2025-03-05 19:47:12 +0300

    sadacha1_05.03.2025

--- Program.cs
+++ Program.cs
@@ -6,29 +6,12 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            //Агрегатные функции
-            int[] numbers = {1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1};
+            List<int> numbers = new List<int> { 1, 3, 5, 7, 9, 2, 4, 6, 8 };
+            var sortnumbers = numbers.OrderByDescending(n => n); // сортировка
+            var otvet = sortnumbers.Where(n => n > numbers.Average());
 
-            //Console.WriteLine(numbers.Sum()); Суммирует
-            //Console.WriteLine(numbers.Average()); Среднее арифмитическое
-            //Console.WriteLine(numbers.Min()); Максимальное значение
-            //Console.WriteLine(numbers.Max()); Минимальное значение
-            //Console.WriteLine(numbers.Length);
-            //Console.WriteLine(numbers.Count()); Сколько всего чисел в массиве
-            //Console.WriteLine(numbers.Count(n => n>5)); Выводить колличество чисел которые больше 5
+            Console.WriteLine(string.Join(" ", otvet));
 
-
-            //Объединение, пересичение и исключение
-            List<int> list1 = new List<int> {1,2,3,4,5};
-            List<int> list2 = new List<int> {4,5,6,7,8};
-
-            //var union = list1.Union(list2); Объединение
-            //var intersection = list1.Intersect(list2); Пересичение
-            //var except = list1.Except(list2); Исключение
-
-            //Console.WriteLine(string.Join(", ", union));
-            //Console.WriteLine(string.Join(", ", intersection));
-            //Console.WriteLine(string.Join(", ", except));
         }
     }
 }

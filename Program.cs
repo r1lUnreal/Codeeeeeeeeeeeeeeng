ecea1744fa71a09547b2b5b9654319dbcf6b03a3
r1lUnreal <150723650+r1lUnreal@users.noreply.github.com>
2025-03-05 18:17:30 +0300

    var

--- Program.cs
+++ Program.cs
@@ -1,58 +1,38 @@
 using System;
 
-namespace ConsoleApp
+class Person
 {
-    public class Program
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
     {
-        public static void Main(string[] args)
+        Name = name;
+        Age = age;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Person> people = new List<Person>
         {
-            //LINQ-запросы
-            int [] numbers = {1,2,3,4,5,6,7,8,9};
-            var even_numbers = numbers.Where(n => n%2==0); //Where (отбор отдельных элементов)
-            Console.WriteLine(string.Join(", ", even_numbers));                       
-            /* foreach(int i in even_numbers)
+            new Person("Фуфелшмерц", 000),
+            new Person("Чубака", 37),
+            new Person("Алёша", 000),
+            new Person("Ольга", 60),
+            new Person("Илья", 15)
+        };
+
+        var groupePeople = people.GroupBy(p => p.Age);
+
+        foreach (var group in groupePeople)
+        {
+            Console.WriteLine($"Возраст: {group.Key}");
+            foreach (var person in group)
             {
-                Console.WriteLine(i);
-            } */
-
-            Console.WriteLine("-----------------------------------------");
-            string[] strings = {"afafas", "aa", "aaaa", "a", "b", "aaaaaaa"};
-            var even_strings = strings.Where(n => n.Length>=2);
-            Console.WriteLine(string.Join(", ", even_strings));
-
-            //SELECT (проекцию - изминение каждого элемента)
-            Console.WriteLine("-----------------------------------------");
-            int [] num = {1,2,3,4,5,6,7,8,9};
-            var squared_num = num.Select(n => n*n);
-            Console.WriteLine(string.Join(" ", squared_num));
-
-            Console.WriteLine("-----------------------------------------");
-            string[] str = {"afafas", "aa", "aaaa", "a", "b", "aaaaaaa"};
-            var select_str = str.Select(n => "!"+n+"!");
-            Console.WriteLine(string.Join(" ", select_str));
-            Console.WriteLine("-----------------------------------------");
-
-            // ORDERBY (сортировка)
-            List<int> number = new List<int> {1,4,5,6,7,1,2,4,7,8,3,2,6};
-            //number.Sort();
-            var orderby_number = number.OrderBy(n => n); //OrderByDescending - в обратном порядке
-            Console.WriteLine(string.Join(" ", orderby_number));
-            Console.WriteLine("-----------------------------------------");
-
-            //GroupBy (группировка по определённому принципу)
-            var names = new List<string> {"Анна", "Андрея", "Борис", "Богдана", "Владимир"};
-            var grouped_names = names.GroupBy(n => n[0]);
-            foreach (var group in grouped_names)
-            {
-                Console.WriteLine(group.Key + ": " +  string.Join(" ", group));
-            }
-
-            Console.WriteLine("-----------------------------------------");
-            int [] nam = {1,2,3,4,5,6,7,8,9};
-            var grouped_nam = nam.GroupBy(n => n%2==0 ? "Пацаны" : "Чушпаны");
-            foreach (var group in grouped_nam)
-            {
-                Console.WriteLine(group.Key + ": " +  string.Join(" ", group));
+                Console.WriteLine($" - {person.Name}");
             }
         }
     }
